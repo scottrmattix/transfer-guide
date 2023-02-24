5d964a9f0d47c3a791003bd327d4a26464eafb6e
Scott Mattix <smattix7D2@gmail.com>
2023-02-24 18:44:54 -0500

    add user types

--- transferguideapp/views.py
+++ transferguideapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import Group, User
 
-# Create your views here.
+
+
+def set_group(request, user_id):
+    if(request.method == 'POST'):
+        group = request.POST.get('usertype', None)
+        g = Group.objects.get(name=group)
+        user = User.objects.get(id=user_id)
+        user.groups.add(g) 
+        user.save()
+    return redirect('home') 
