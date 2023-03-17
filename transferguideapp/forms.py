a2b51f79b362b0224f4a74ca4363f4c3e7310591
Scott Mattix <smattix7D2@gmail.com>
2023-03-17 18:31:53 -0400

    add dynamic form

--- transferguideapp/forms.py
+++ transferguideapp/forms.py
@@ -1,33 +1,14 @@
-from django.forms import ModelChoiceField, ModelForm, Form, CharField
-from transferguideapp.models import CourseTransfer, ExternalCollege, InternalCourse, ExternalCourse
+from django.forms import ChoiceField, ModelChoiceField, Form, CharField, RadioSelect
 
-"""
-class ExternalCollegeForm(ModelForm):
-    class Meta:
-        model = ExternalCollege
-        fields = ["college_name"]
 
-class ExternalCourseForm(ModelForm):
-    class Meta:
-        model = ExternalCourse 
-        fields = ["college", "mnemonic", "course_number", "course_name"]
-        field_classes = {
-                "college" : ExternalCollegeForm,
-        }
 
-class InternalCourseForm(ModelForm):
-    class Meta:
-        model = InternalCourse
-        fields = ["mnemonic", "course_number", "course_name"]
-"""
 
 # temporary solution, the form should be more dynamic to accept both already register courses as well as new courses
 class TransferRequestForm(Form):
-    external_course_name = CharField()
-    external_course_college = CharField()
-    external_course_mnemonic = CharField()
-    external_course_number = CharField()
-
-    internal_course_name = CharField()
-    internal_course_mnemonic = CharField()
-    internal_course_number = CharField()
+    def __init__(self, queryset, *args, **kwargs):
+        self.external_course_name = CharField()
+        self.external_course_college = CharField()
+        self.external_course_mnemonic = CharField()
+        self.external_course_number = CharField()
+        self.internal_course_id = ModelChoiceField(queryset=queryset)
+        super().__init__(*args, **kwargs)
