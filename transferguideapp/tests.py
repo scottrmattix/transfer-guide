f49344d628d6294ebfd6c37e6ba86e14c911c37b
Scott Mattix <smattix7D2@gmail.com>
2023-03-18 16:11:16 -0400

    test method headers

--- transferguideapp/tests.py
+++ transferguideapp/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 
-
+# This test is just to verify that CI is working
 class TestFail(TestCase):
     def test_fail(self):
         self.assertTrue(True)
+
+
+class TestModelCreation(TestCase):
+    def test_external_college_create():
+    def test_external_course_create():
+    def test_internal_course_create():
