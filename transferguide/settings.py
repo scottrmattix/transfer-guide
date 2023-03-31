c323d393470cac2d9e93a5001fc4b243cf02da2a
camccaffrey <89548503+camccaffrey@users.noreply.github.com>
2023-03-31 17:05:54 -0400

    changed database settings

--- transferguide/settings.py
+++ transferguide/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'transferguide.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -103,7 +103,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
