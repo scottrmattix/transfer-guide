649512329965e874e3a99c48fc4b889dea58164d
qdq5wa <76853830+qdq5wa@users.noreply.github.com>
2023-04-14 13:01:38 -0400

    delete file not part of production

--- DBpop4localtests.py
+++
@@ -1,6 +0,0 @@
-from transferguideapp.models import InternalCourse, ExternalCollege, ExternalCourse, Favorites, CourseTransfer, TransferRequest
-
-ExternalCollege(college_name="Virginia Tech", domestic_college=True).save()
-ExternalCourse(college=ExternalCollege.objects.first(), mnemonic="MATH", course_number = "0000", course_name="blah").save()
-InternalCourse(id=1, mnemonic="MATH", course_number="1111", course_name="halb", credits=3).save()
-CourseTransfer(external_course=ExternalCourse.objects.last(), internal_course=InternalCourse.objects.last()).save()
