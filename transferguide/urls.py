f870b7d86cee5813f5bac492127408a0616b8229
qdq5wa <76853830+qdq5wa@users.noreply.github.com>
2023-04-15 19:15:12 -0400

    Update urls.py

--- transferguide/urls.py
+++ transferguide/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
-from transferguideapp.views import favorite_request, add_external_college, set_group, submit_transfer_request, account_info, favorites, add_favorite, delete_favorite, update_favorites, CourseSearch, submit_search, InternalCoursePage, ExternalCoursePage, UpdateInternal, UpdateExternal, UpdateCourses, submit_update, make_request, CourseRequest, HandleRequests, accept_request, reject_request, delete_request, ProfilePage, sis_lookup, , add_to_cart, cart_TR
+from transferguideapp.views import favorite_request, add_external_college, set_group, submit_transfer_request, account_info, favorites, add_favorite, delete_favorite, update_favorites, CourseSearch, submit_search, InternalCoursePage, ExternalCoursePage, UpdateInternal, UpdateExternal, UpdateCourses, submit_update, make_request, CourseRequest, HandleRequests, accept_request, reject_request, delete_request, ProfilePage, sis_lookup, add_to_cart, cart_TR
 
 urlpatterns = [
     path('admin/', admin.site.urls),
